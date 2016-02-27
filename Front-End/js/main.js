5906ab9a9bcb67ce0e2dadcf8d62d47a077eae21
ShadoDart <jared.bledsoe@icloud.com>
2016-02-27 12:56:36 -0600

    New Features
    Responsive NavBar, and new animations.

--- Front-End/js/main.js
+++ Front-End/js/main.js
@@ -1,10 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function(){
+	$('#mobileNav').hide();
+	$('#comment1').hide();
+	$('#comment1').delay(1000).fadeIn(500);
+
+ $('#highlightAnimation')
+      .delay(1000)
+      .queue( function(next){ 
+        $(this).css("background-color", "#C6D53E"); 
+        next(); 
+      });
+
+// Show correct navBar for device
+	if($(window).width() <= 768){
+	$('#navBar').hide();
+	$('#mobileNav').show();
+	}
 
 
-$("jumper").click(function() {
-    $("html,body").animate({
-        scrollTop: $("#searchSpot").offset().top},
-        "slow");
-    debugger;
+
+
+
+
+// Typewriter
+	var str = "<p>PeerMarked</p>",
+    i = 0,
+    isTag,
+    text;
+
+(function type() {
+    text = str.slice(0, ++i);
+    if (text === str) return;
+    
+    document.getElementById('typewriter').innerHTML = text;
+
+    var char = text.slice(-1);
+    if( char === '<' ) isTag = true;
+    if( char === '>' ) isTag = false;
+
+    if (isTag) return type();
+    setTimeout(type, 80);
+}());
+
+
 });
-});
+
