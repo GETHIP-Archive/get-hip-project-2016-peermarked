7290d9cc1f72ddb4a5a78dd07f0b71eb00d6047c
ShadoDart <jared.bledsoe@icloud.com>
2016-03-01 21:55:24 -0600

    More Features
    Much more responsive, changed fonts/styles, cleaner look.

--- Front-End/js/main.js
+++ Front-End/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	$('#mobileNav').hide();
-	$('#comment1').hide();
-	$('#comment1').delay(1000).fadeIn(500);
+    $('#comment1').hide();
 
  $('#highlightAnimation')
       .delay(1000)
@@ -16,13 +15,17 @@ $(document).ready(function(){
 	$('#mobileNav').show();
 	}
 
+    if($(window).width() > 768) {
+        $('#comment1').delay(1000).fadeIn(500);
+    }
+
 
 
 
 
 
 // Typewriter
-	var str = "<p>PeerMarked</p>",
+	var str = '<p>PeerMarked</p>',
     i = 0,
     isTag,
     text;
@@ -40,6 +43,65 @@ $(document).ready(function(){
     if (isTag) return type();
     setTimeout(type, 80);
 }());
+
+
+
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var d = 0; d < dropdowns.length; d++) {
+      var openDropdown = dropdowns[d];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
+
+
+// Type once in sight
+// if(aboutTyper)return;
+
+// var aboutTyper = false;
+
+// $(window).scroll(function() {
+//     if ($('#aboutSpot').is(':visible')) {
+
+//     var str = "<p>About</p>",
+//     i = 0,
+//     isTag,
+//     text;
+
+// (function type() {
+//     text = str.slice(0, ++i);
+//     if (text === str) return;
+    
+//     document.getElementById("typewriter").innerHTML = text;
+
+//     var char = text.slice(-1);
+//     if( char === '<' ) isTag = true;
+//     if( char === '>' ) isTag = false;
+
+//     if (isTag) return type();
+//     setTimeout(type, 80);
+
+//     var aboutTyper = false;
+// }());
+
+//     }
+// });
+
 
 
 });
